5268aa2487ff8cc5868c770072e7f9efbd563429
phongnbn <phongnbn123@gmail.com>
2025-11-05 23:16:01 +0700

    hoan thien trang chi tiet

--- src/pages/client/home/HomePage.jsx
+++ src/pages/client/home/HomePage.jsx
@@ -19,6 +19,8 @@ import posterQuanKy from "../../../assets/images/poster/quan-ky-nam.jpg";
 import posterHoangTu from "../../../assets/images/poster/hoang-tu-quy.png";
 import posterBaDung from "../../../assets/images/poster/ba-dung-buon-con.png";
 import posterCoHau from "../../../assets/images/poster/co-hau-gai.jpg";
+import { useNavigate } from "react-router-dom";
+
 
 const { Search } = Input;
 const { Option } = Select;
@@ -27,6 +29,7 @@ const { Text, Link } = Typography;
 
 const DEFAULT_POSTER = posterTraiTim;
 const DEFAULT_BANNER = bannerImg;
+
 
 const BANNER_URL = bannerImg;
 
@@ -168,6 +171,7 @@ const styles = {
 };
 
 const HomePage = () => {
+  const navigate = useNavigate();
   const [tabKey, setTabKey] = useState("coming");
   const [movies] = useState(sampleMovies);
   const [query, setQuery] = useState("");
@@ -297,12 +301,13 @@ const HomePage = () => {
                   </div>
 
                   <div style={{ paddingTop: 8 }}>
-                    <Link
-                      style={styles.titleLink}
-                      onClick={() => alert(`Mở chi tiết: ${m.title}`)}
-                    >
-                      {m.title}
-                    </Link>
+                  <Link
+  style={styles.titleLink}
+  onClick={() => navigate(`/showtime/${m.id}`)}
+>
+  {m.title}
+</Link>
+
                     <div style={styles.metaSmall}>
                       Thể loại: <Text strong>{m.genres.join(", ")}</Text>
                     </div>
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../common/layouts/MainLayout";
 import LoginPage from "../pages/client/auth/LoginPage";
 import RegisterPage from "../pages/client/auth/RegisterPage";
 import HomePage from "../pages/client/home/HomePage";
+import ShowtimePage from "../pages/client/ShowTimePage"; 
 
 export const MainRoutes = [
   {
@@ -12,6 +13,10 @@ export const MainRoutes = [
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: "showtime/:id", 
+        element: <ShowtimePage />,
       },
     ],
   },
@@ -25,8 +30,8 @@ export const MainRoutes = [
       },
       {
         path: "login",
-        element : <LoginPage />
-      }
+        element: <LoginPage />,
+      },
     ],
   },
 ];
